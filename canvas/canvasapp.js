0ade2bfdd4ba1c1662bb28ead20b47f83054e8da
Kathleen Votolato <kvotolato3@gmail.com>
2014-06-05 17:50:13 -0400

    Add geoApp and canvasApp

---
+++ canvas/canvasapp.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+  geoApp.draw();
+});
+
+var geoApp = {
+  draw: function(){
+    var canvas = $('#tutorial')[0];
+      if (canvas.getContext){
+        var ctx = canvas.getContext('2d');
+        ctx.fillStyle = "rgb(200,0,0)";
+        ctx.fillRect (10, 10, 55, 50);
+
+        ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+        ctx.fillRect (30, 30, 55, 50);
+      }
+  }
+};
+
+
